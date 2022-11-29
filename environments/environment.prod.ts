e103e76ccd1136b771948fc3fb2d37bd2aa3f695
nitishtrivedi <nitish_trivedi0194@yahoo.com>
2022-11-29 18:12:00 +0530

    CHanged ENVs to point to RENDER

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    apiURL: 'https://pinkbits-backend.herokuapp.com/api/v1/'
-  };
-  
+    apiURL: 'https://pinkbits-backend.onrender.com/api/v1/'
+};
